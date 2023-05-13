f67fc23e2a51f32abdc68f0fc2810492b0a0c0ed
gostek231 <szkulenko@gmail.com>
2023-05-13 21:14:17 +0200

    kolejne...

--- budzet/views.py
+++ budzet/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-
 from .models import Post
 
-# Create your views here.
 def post_list(request):
     posts = Post.objects.filter(publish_Date__lte=timezone.now()).order_by('publish_Date')
     return render(request, 'budzet/post_list.html', {'posts': posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'budzet/post_detail.html', {'posts': post})
 
 
