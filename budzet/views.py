dc8cbc3e77d0b68ac38bd80a0b487d23d714220f
gostek231 <szkulenko@gmail.com>
2023-05-13 14:15:24 +0200

    szablon html commit

--- budzet/views.py
+++ budzet/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'budzet/post_list.html', {})
+    posts = Post.objects.filter(publish_Date__lte=timezone.now()).order_by('publish_Date')
+    return render(request, 'budzet/post_list.html', {'posts': posts})
 
 
 
